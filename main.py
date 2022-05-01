4972799471ca82c81011c8e716e199566f96a554
Nowshad Ruhani <nowshad.cse@gmail.com>
2022-05-01 22:10:14 +0600

    passlib library add

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
+from enum import unique
 from fastapi import FastAPI, Depends
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from tortoise.models import Model
+from tortoise import fields
+from tortoise.contrib.fastapi import register_tortoise
+from tortoise.contrib.pydantic import pydantic_model_creator
 
 app = FastAPI()
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl = 'token')
+class User(Model):
+    id = fields.IntField(pk=True)
+    username = fields.CharField(50, unique=True)
+    password_hash = fields.CharField(255)
 
-@app.post('/token')
-async def token(form_data: OAuth2PasswordRequestForm = Depends()):
-    return {'access_token': form_data.username + 'token'}
+    @classmethod
+    async def get_user(cls, username):
+        return cls.get(username=username)
 
-@app.get('/')
-async def index(token: str = Depends(oauth2_scheme)):
-    return {'the_token': token}
+    def verify_password(self, password):
+        return True
+
+register_tortoise(
+    app,
+    db_url='sqlite://db.sqlite3',
+    modules={'models': ['main']},
+    generate_schemas=True,
+    add_exception_handlers=True
+)
 
 
