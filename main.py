467c3aceffcab264587dfc7df154d82093bfcdda
Nowshad Ruhani <nowshad.cse@gmail.com>
2022-05-02 04:41:53 +0600

    code level update

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import jwt
 from enum import unique
 from fastapi import FastAPI, Depends
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
@@ -9,10 +10,11 @@ from tortoise.contrib.pydantic import pydantic_model_creator
 
 app = FastAPI()
 
+JWT_SECRET = 'myjwtsecret'
+
 class User(Model):
     id = fields.IntField(pk=True)
     username = fields.CharField(50, unique=True)
-    email = fields.CharField(255)
     password_hash = fields.CharField(255)
 
     @classmethod
@@ -25,9 +27,34 @@ class User(Model):
 User_Pydantic = pydantic_model_creator(User, name='User')
 UserIn_Pydantic = pydantic_model_creator(User, name='UserIn', exclude_readonly=True)
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
+
+async def authenticate_user(username: str, password: str):
+    user = await User.get(username=username)
+    if not user:
+        return False
+    if not user.verify_password(password):
+        return False
+    return user
+    
+
+
+@app.post('/token')
+async def generate_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = await authenticate_user(form_data.username, form_data.password)
+
+    if not user: 
+        return {'error': 'invalid credentials'}
+    
+    user_obj = await User_Pydantic.from_tortoise_orm(user)
+
+    token = jwt.encode(user_obj.dict(), JWT_SECRET)
+    return {'access_token': token, 'token_type': 'bearer'}
+
+
 @app.post('/users')
 async def create_user(user: UserIn_Pydantic):
-    user_obj = User(username=user.username, email=user.email, password_hash=bcrypt.hash(user.password_hash))
+    user_obj = User(username=user.username, password_hash=bcrypt.hash(user.password_hash))
     await user_obj.save()
     return await User_Pydantic.from_tortoise_orm(user_obj)
 
