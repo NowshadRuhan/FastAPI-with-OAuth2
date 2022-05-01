c7faa24555e710cd5720ed4a405526f32408e858
Nowshad Ruhani <nowshad.cse@gmail.com>
2022-05-02 04:17:57 +0600

    add PYJWT library

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ app = FastAPI()
 class User(Model):
     id = fields.IntField(pk=True)
     username = fields.CharField(50, unique=True)
+    email = fields.CharField(255)
     password_hash = fields.CharField(255)
 
     @classmethod
@@ -19,14 +20,16 @@ class User(Model):
         return cls.get(username=username)
 
     def verify_password(self, password):
-        return True
+        return bcrypt.verify(password, self.password_hash)
 
 User_Pydantic = pydantic_model_creator(User, name='User')
 UserIn_Pydantic = pydantic_model_creator(User, name='UserIn', exclude_readonly=True)
 
 @app.post('/users')
 async def create_user(user: UserIn_Pydantic):
-    user_obj = User(username=user.username, password_hash=bcrypt.hash(user.password_hash))
+    user_obj = User(username=user.username, email=user.email, password_hash=bcrypt.hash(user.password_hash))
+    await user_obj.save()
+    return await User_Pydantic.from_tortoise_orm(user_obj)
 
 
 register_tortoise(
